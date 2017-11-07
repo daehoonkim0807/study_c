58b6b7e011867da38f372e2673f4b124eba944e5
Daehoon Kim <kdh930807@gmail.com>
2017-11-07 02:53:34 +0000

    <study1>

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,32 +1,61 @@
 #include <iostream>
-int five_count(int n);
+//void SimpleFunc(int n);
+//void SimpleFuncPtr(int* param);
+//void Calcul(int* sum, int* multiply);
 
-int main() {
-    int n1, n2, n3;
-    int s1, s2, s3, sum;
-    std::cin >> n1 >> n2 >> n3;
+
+
+int main(int argc, char** argv)
+{
+    int arr[10] = {1,2,3,4,5,6,7,8,9,0};
     
-    s1 = five_count(n1);
-    s2 = five_count(n2);
-    s3 = five_count(n3);
+    std::cout << "ss" << std::endl;
+    /*
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *ptr = arr;
+    for (int i = 0 ; i < 5; i++)
+    {
+        *(ptr+i) += 2;
+    }
+    for (int i = 0 ; i < 5; i++)
+    {
+        std::cout<< arr[i] << std::endl;
+    }
+    */
     
-    sum = s1 + s2 + s3;
+    /*
+    int sum;
+    int multiply;
     
-    //std::cout << s1 << s2<< s3;
-    std::cout << sum;
+  
+    Calcul(&sum, &multiply);
     
+    std::cout << sum << "\n" << multiply;    
+    */
+    /*
+    int num = 0;
+    SimpleFunc(num);
+    std::cout << "After SimpleFunc : " << num << std::endl;
+    
+    SimpleFuncPtr(&num);
+    std::cout << "After SimpleFuncPtr: " << num << std::endl;
+    */
     
     return 0;
-}   
-
-int five_count(int n){
-    int count = 0; 
-    int f = n/100;
-    int s = (n%100)/10;
-    int t = (n%100)%10;
-    if (f==5) count += 1;
-    if (s==5) count += 1;
-    if (t==5) count += 1;
-    //std::cout << t <<std::endl;
-    return count;
 }
+
+void Copy(int arr[]){
+    
+}
+
+
+/*
+void Calcul(int* sum, int* multiply)
+{
+    int n1, n2;
+    std::cin >> n1 >> n2;
+    *sum = n1 + n2;
+    *multiply = n1*n2;
+
+}
+*/
